221e64b610514e5f7ae1a73e4843a7ac9bbff74d
Alan Carl Brown <stretch@gmail.com>
2012-08-22 21:15:45 -0700

    Refactored for simplicity.

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,24 +1,19 @@
 class DatabasesController < ApplicationController
   def index
-    excludes = ['.', '..', 'modelizer']
-    
-    @applications = Dir.new(apps_dir_path)
-
-    @readable_apps = Array.new
-
-    # Find all apps with readable config/database.yml
-    @applications.each do |application|
-      if !excludes.include?(application)
-        apps_path = apps_dir_path + "/" + application
-        db_config_file = apps_path + "/config/database.yml"
-
-        if File.readable_real?(db_config_file)
-          @readable_apps << application
-        end
-      end
+    # Find all apps with readable db config
+    @readable_apps = get_applications.select do |application|
+      File.readable_real?(apps_dir_path + "/" + application + "/config/database.yml")
     end
   end
 
+  private
+  
+  def get_applications
+    excludes = ['.', '..', 'modelizer']
     
+    applications = Dir.new(apps_dir_path)
 
+    # Find all apps with readable config/database.yml
+    applications.select {|application| !excludes.include?(application)}
+  end
 end
