5fbb757e0713769fb3fe6b7fc87ca84de59d3965
Alan Carl Brown <stretch@gmail.com>
2012-08-22 21:06:56 -0700

    Moved presentation of table collection from databases/show to tables/index.

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -19,24 +19,6 @@ class DatabasesController < ApplicationController
     end
   end
 
-  def show
-    @application = params['application']
-
-    Schema.establish_connection(get_db_config)
     
-    # Identify the tables in the database for this application
-    begin
-      @tables = Schema.connection.tables - ["schema_info", "schema_migrations", "sessions"]
 
-      if @tables.length > 0
-        # Get the button sets
-        @button_set = @tables.collect do |table|
-          {table: table, application: @application}
-        end
-      end
-    rescue
-      flash[:notice] = "The database for #{@application} could not be accessed."
-      redirect_to :action => 'index'
-    end
-  end  
 end
--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,4 +1,12 @@
 class TablesController < ApplicationController
+  def index
+    @application = params['application']
     
+    begin
+      @tables = tables(@application)
+    rescue
+      flash[:notice] = "The database for #{@application} could not be accessed."
+      redirect_to controller: 'databases', action: 'index'
+    end
   end
 end
