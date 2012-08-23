1744e682d2709ca063453987ac0ec2987b7086fc
Alan Carl Brown <stretch@gmail.com>
2012-08-22 20:58:21 -0700

    Moved databases/show to tables/index.
    This is a collection of tables for a database, not a single resource, so index is more appropriate.

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,6 +1,10 @@
 class AssociationsController < ApplicationController
   layout false
 
+  def index
+    @table = params['table']
+    @application = params['application']
 
+    @associatable_columns = get_associations(@table, @application)
   end
 end
--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,15 +1,4 @@
 class TablesController < ApplicationController
-  def show
-    # Identify the tables in the database for this application
-    # @tables = eval("#{get_app_model}.connection.tables")
-    Schema.establish_connection(get_db_config)
-    @tables = Schema.connection.tables
-    @table = params['table']
     
-    @associatable_column_names = Array.new
-    counter = 0
-    get_associations(@table, @tables, counter)
-  end
-
   end
 end
